ea4d2589b00e0fbeedf740024983b7cb53eb0e5d
waxtdm <5211yy@163.com>
2025-03-14 16:36:30 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# zhtyh
+ zhtyh
---
+++ rhb/本地【超大杯】/lib/qyg0.js
@@ -0,0 +1,7 @@
+{
+  "api_service": "https://852b95d36.xwlpk.cn/api/",
+  "help_url": "https://kayleigh.xtyzgx.cn/help",
+  "points_mall_url": "https://kayleigh.xtyzgx.cn/",
+  "short_video_service": "https://kayleigh.xtyzgx.cn/video/display",
+  "gateway_websocket_url": "wss://app-wss.ecoliving168.com/wss"
+}
